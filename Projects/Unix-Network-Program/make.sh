fccadb4b793e4865ec7fccd048d2f3e535243f72
wangyingwu <wang_ying_wu@qq.com>
2019-11-26 09:22:38 +0800

    update sh

--- Projects/Unix-Network-Program/make.sh
+++ Projects/Unix-Network-Program/make.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # build client.c
 cd ./lib
-cmake . & make
+cmake .
+make
 cd ../cli_dir
-cmake . & make
+cmake .
+make
 cd ../srv_dir
-cmake . & make
+cmake .
+make
 cd ../
 g++ -g -w -o client cli_dir/libcli.a lib/libutils.a -std=c++11 -lpthread
 g++ -g -w -o server srv_dir/libsrv.a lib/libutils.a -std=c++11 -lpthread
